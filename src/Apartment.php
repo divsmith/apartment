74644c4dee2a8c65b3b24286904d0228b3ddca45
Parker Smith <parker@parkersmith.us>
2015-05-26 03:30:51 -0600

    Add apartment middleware

---
+++ src/Apartment.php
@@ -0,0 +1,43 @@
+<?php  namespace Apartment; 
+
+use Closure;
+use Illuminate\Contracts\Container\Container;
+use Tenantable\Tenantable;
+
+class Apartment {
+
+    /**
+     * @var \Tenantable\Tenantable
+     */
+    protected $tenantable;
+
+    /**
+     * @var \ \Illuminate\Contracts\Container\Container
+     */
+    protected $app;
+
+    public function __construct(Tenantable $tenantable, Container $app)
+    {
+        $this->tenantable = $tenantable;
+        $this->app = $app;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if( $this->tenantable->tenantHasUser())
+        {
+            $request->tenantId = $this->tenantable->getTenantId();
+
+            return $next($request);
+        }
+
+        $this->app->abort(403, 'Unauthorized User');
+    }
+}
